8574b658819fd6ab157e6322258398a41d60f927
bishnu133 <bishnuprasadp@gmail.com>
2026-05-01 11:46:11 +0800

    refactoring the testcases ,adding json file to read the data from testfile

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,35 +1,30 @@
+import {Page, Locator , expect} from '@playwright/test'
 
-import { Page, Locator } from "@playwright/test";
+export class loginpage
+{
 
-
- export class loginpage{
-    //Because the class needs to know which browser page it should work on.
- //class variable, I am declaring a page variable inside the class.
- private page:Page;
-private username:Locator;
-private password:Locator;
-private loginbtn:Locator;
-
-
+    //class Variables
+    private page: Page;
+    private username: Locator;
+    private password: Locator;
+    private loginbtn: Locator;
+    private errorMsg:Locator;
 
 //constructors
-constructor(page:Page) // receives page from test
+constructor(page:Page)
 {
-    // stores received page into class variable,I am receiving the real page from the test and saving it inside this class.
     this.page=page;
-     //locators
-this.username= page.locator("input#user-name");
-this.password=page.locator("input#password");
-this.loginbtn=page.locator('input[value="Login"]');
-    
+    this.username= page.locator("input#user-name");
+    this.password= page.locator("input#password");
+    this.loginbtn= page.locator('input[value="Login"]');
+    this.errorMsg=page.locator("h3[data-test='error']");
 }
-  
-
 
 //methods
 async openURL()
 {
-  await this.page.goto("/");
+      await this.page.goto("/");
+      //await page.goto("/");
 }
 
 async loginToApp(username:string,password:string)
@@ -37,19 +32,12 @@ async loginToApp(username:string,password:string)
 await this.username.fill(username);
 await this.password.fill(password);
 await this.loginbtn.click();
- // await page.waitForTimeout(10000);
-}
 }
 
 
 
-
-
-
-
-
-
- 
-
-
-  
+  getErrorMessage() {
+    return this.errorMsg;
+  }
+    
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,39 @@
-import { test,expect } from 'playwright/test'
+import{ test, expect} from '@playwright/test'
 
-import { loginpage } from "../pages/loginPage";
-test("Scenario-1:swaglabs Valid login test", async ({ browser }) => {
-  const page = await browser.newPage();
-  const login_page=new loginpage(page);
-  login_page.openURL();
-  login_page.loginToApp("standard_user", "secret_sauce");
-await page.waitForTimeout(10000);
+import{ loginpage} from '../pages/loginPage'
+import LoginData from '../test-data/loginData.json'
+
+test("Scenario-1: swaglabs Valid login test", async({page})=>
+{
+
+const login_page=new loginpage(page);
+await login_page.openURL();
+await login_page.loginToApp(LoginData.validUser.Username,LoginData.validUser.Password)
+console.log("valid Username: "+LoginData.validUser.Username);
+console.log("valid password :"+LoginData.validUser.Password);
+//page.waitForTimeout(10000);
+
+ //verify user is landed on the product page
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+
+});
+
+
+test("Scenario-2: swaglabs InValid login test", async({page})=>
+{
+
+const login_page=new loginpage(page);
+await login_page.openURL();
+await login_page.loginToApp(LoginData.invalidUser.Username,LoginData.invalidUser.Password)
+console.log("invalid Username: "+LoginData.invalidUser.Username);
+console.log("invalid password :"+LoginData.invalidUser.Password);
+//page.waitForTimeout(10000);
+
+  
+  await expect(login_page.getErrorMessage()).toBeVisible();
+
+  await expect(login_page.getErrorMessage()).toHaveText("Epic sadface: Username and password do not match any user in this service"
+  );
+    
 
 });
